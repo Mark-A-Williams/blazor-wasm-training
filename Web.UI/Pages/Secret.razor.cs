60cfaf6a3299e2b58621f794e524a50518fc65df
Mark Williams <mark.williams@enable.com>
2021-01-06 15:16:55 +0000

    Scoped styles and component

---
+++ Web.UI/Pages/Secret.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+
+namespace Web.UI.Pages
+{
+    public partial class Secret
+    {
+        [Inject]
+        protected IJSRuntime _ijsRuntime { get; set; }
+
+        private void OnClick()
+        {
+            var isWebAssembly = _ijsRuntime is IJSInProcessRuntime;
+
+            if (isWebAssembly)
+            {
+                Console.WriteLine("The app is running on WebAssembly");
+            }
+            else
+            {
+                Console.WriteLine("The app is running on sever-side");
+            }
+
+        }
+    }
+}
