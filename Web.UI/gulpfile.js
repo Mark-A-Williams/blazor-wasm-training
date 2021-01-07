46e2c7959eda7b67ee502a93e8422741fae196b5
Mark Williams <mark.williams@enable.com>
2021-01-07 17:42:09 +0000

    Bring back SCSS with Gulp

---
+++ Web.UI/gulpfile.js
@@ -0,0 +1,36 @@
+﻿/// <binding ProjectOpened='default, watch' />
+"use strict";
+
+const
+    gulp = require("gulp"),
+    autoprefixer = require("gulp-autoprefixer"),
+    changed = require("gulp-changed"),
+    rename = require("gulp-rename"),
+    sass = require("gulp-sass");
+
+const scssPaths = ["*/**/*.scss", "Areas/**/*.scss"];
+
+exports.default = gulp.parallel(sassCompile);
+exports.watch = gulp.parallel(sassWatch);
+
+function sassCompile(done) {
+    var ext = ".css";
+    var dest = function (file) {
+        return file.base;
+    };
+
+    return gulp.src(scssPaths)
+        .pipe(changed(dest, { extension: ext }))
+        .pipe(sass({ outputStyle: "expanded" }).on("error", done))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(rename({
+            extname: ext
+        }))
+        .pipe(gulp.dest(dest));
+}
+
+function sassWatch() {
+    return gulp.watch(scssPaths, gulp.series(sassCompile));
+}
